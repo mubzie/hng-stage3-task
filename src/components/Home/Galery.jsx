cb81e5eef2c8a6601ab061add20378599339c9ce
mubzie <85705074+mubzie@users.noreply.github.com>
2023-09-19 12:20:44 +0100

    working on gallery page

---
+++ src/components/Home/Galery.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "./Galery.module.css";
+
+const Gallery = () => {
+  const [gallery, setGallery] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const countryCount = [];
+
+    const generateRandom = () => {
+      for (let i = 0; i < 15; i++) {
+        countryCount.push(i);
+      }
+    };
+
+    const fetchImages = async () => {
+      let resultArray = [];
+
+      const response = await fetch(`https://restcountries.com/v3.1/all`);
+      const data = await response.json();
+
+      generateRandom();
+
+      countryCount.map((index) => {
+        resultArray = [
+          ...resultArray,
+          {
+            id: index,
+            name: data[index]["name"]["common"],
+            tag: data[index]["continents"][0],
+            image: data[index]["flags"]["png"],
+          },
+        ];
+      });
+
+      setGallery([...resultArray]);
+    };
+
+    fetchImages();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.cardWrapper}>
+        {gallery.map((item) => {
+          return (
+            <div key={item.id} className={styles.parentContainer}>
+              <div className={styles.cardContainer}>
+                <div className={styles.card}>
+                  <img
+                    className={styles.gallery}
+                    style={{ backgroundImage: `url(${item.image})` }}
+                  ></img>
+                  <div className="galleryInfo">
+                    <div className="name">{item.name}</div>
+                    <div className="tag">{item.tag}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Gallery;
---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyn-EU5XL1ZKgv8PXqp6rXD3PoNYy-2hs",
+  authDomain: "hng-stage3-project.firebaseapp.com",
+  projectId: "hng-stage3-project",
+  storageBucket: "hng-stage3-project.appspot.com",
+  messagingSenderId: "637349643104",
+  appId: "1:637349643104:web:aa995dc56d61e055109405",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
