ee1e6da71d33f15a6721c60356496097c19a1678
mubzie <85705074+mubzie@users.noreply.github.com>
2023-10-29 07:35:43 +0100

    added an env file to manage firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDyn-EU5XL1ZKgv8PXqp6rXD3PoNYy-2hs",
-  authDomain: "hng-stage3-project.firebaseapp.com",
-  projectId: "hng-stage3-project",
-  storageBucket: "hng-stage3-project.appspot.com",
-  messagingSenderId: "637349643104",
-  appId: "1:637349643104:web:aa995dc56d61e055109405",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 //initialize Firebase
